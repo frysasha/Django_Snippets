c881e36cd5c9dfaee81398e993b1f8d32f39a32c
frysasha <frysasha@yandex.ru>
2022-05-25 13:47:46 +0300

    add actions on snippets

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,4 +1,4 @@
-from django.http import Http404
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from MainApp.models import Snippet
 from MainApp.forms import SnippetForm
@@ -23,6 +23,37 @@ def add_snippet_page(request):
             'form': form
         }
         return render(request, 'pages/add_snippet.html', context)
+
+
+def edit_snippet(request, s_id):
+    try:
+        snippet = Snippet.objects.get(pk=s_id)
+        form = SnippetForm(request.POST)
+        context = {
+            'pagename': 'Изменение сниппета',
+            'form': form,
+            'snippet': snippet
+        }
+        if request.method == "POST":
+            snippet.name = request.POST.get('name')
+            snippet.lang = request.POST.get('lang')
+            snippet.code = request.POST.get('code')
+            snippet.save()
+
+            return HttpResponseRedirect("/")
+        else:
+            return render(request, 'pages/snippet_edit_page.html', context)
+    except Snippet.DoesNotExist:
+        return HttpResponseNotFound('Snippet not found')
+
+
+def delete_snippet(request, s_id):
+    try:
+        snippet = Snippet.objects.get(pk=s_id)
+        snippet.delete()
+        return redirect("snippets_list")
+    except Snippet.DoesNotExist:
+        return HttpResponseNotFound('Snippet not found')
 
 
 def snippets_page(request):
--- Snippets/urls.py
+++ Snippets/urls.py
@@ -23,5 +23,7 @@ urlpatterns = [
     path('snippets/add', views.add_snippet_page, name='snippets_add'),
     path('snippets/list', views.snippets_page, name='snippets_list'),
     path('snippets/<int:s_id>', views.snippet_page, name='snippet_page'),
+    path('snippets/edit/<int:s_id>', views.edit_snippet, name='snippet_edit'),
+    path('snippets/delete/<int:s_id>', views.delete_snippet, name='snippet_delete'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
